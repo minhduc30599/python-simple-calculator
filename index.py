18eb501a80348155c5a96bb1d814d2b1a54bfe1c
Minh Duc Nguyen <minhduc3051999@gmail.com>
2022-09-30 14:53:41 +0700

    add comment and fix bug when user type wrong answer repeat input

--- index.py
+++ index.py
@@ -21,9 +21,10 @@ def calculate_number(first_num, second_num, oper):
         clear()
 
     return result
-    
+
 print(logo)
 
+# ask user numbers and operator for calculating
 first_number = float(input('What\'s the first number? '))
 print('+\n-\n*\n/\n')
 operator = input('Pick an operation ')
@@ -31,15 +32,22 @@ second_number = float(input('What\'s the next number? '))
 
 first_result = calculate_number(first_number, second_number, operator)
 
+# print first answer
 print(f'{first_number} {operator} {second_number} = {first_result}')
 
-y_or_n = input(f"Type 'y' to continue calculating with {first_result}, or type 'n' to start a new calculation: ")
+# keep calculating or not
+while True:
+    y_or_n = input(f"Type 'y' to continue calculating with {first_result}, or type 'n' to start a new calculation: ")
+    if y_or_n == 'n':
+        clear()
+        first_result = 0
+        flag = True
+        break
+    elif y_or_n == 'y':
+        flag = False
+        break
 
-if y_or_n == 'n':
-    clear()
-    first_result = 0
-    flag = True
-        
+# calculating second answer and ask user keep calculating or not
 while not flag:
     operator = input('Pick an operation ')
     next_number = float(input('What\'s the next number? '))
@@ -48,11 +56,15 @@ while not flag:
 
     print(f'{first_result} {operator} {next_number} = {new_result}')
 
-    y_or_n = input(f"Type 'y' to continue calculating with {new_result}, or type 'n' to start a new calculation: ")
-
-    if y_or_n == 'n':
-        clear()
-        first_result = 0
-        flag = True
+    while True:
+        y_or_n = input(f"Type 'y' to continue calculating with {new_result}, or type 'n' to start a new calculation: ")
+        if y_or_n == 'n':
+            clear()
+            first_result = 0
+            flag = True
+            break
+        elif y_or_n == 'y':
+            flag = False
+            break
     
     first_result = new_result
